89ae47c10fa5da7267fa7052076653cae7462499
Sidney Cadot <sidney@jigsaw.nl>
2016-03-13 13:45:26 +0100

    Improved internet player: - signals/slots - proper control-C handling - nicer messages

--- PlayInternetRadio.py
+++ PlayInternetRadio.py
@@ -1,64 +1,111 @@
 #! /usr/bin/env python3
 
-import time
-import socket
-import subprocess
-import re
+import time, socket, re, subprocess, logging, math
+
 from LedDisplay import LedDisplay
-import logging
 
-logger = logging.getLogger(__name__)
-
-class MP3Player:
+class Signal:
+    """ A very lightweight implementation of the Signal/Slot mechanism.
+    """
     def __init__(self):
-        self.process = subprocess.Popen(args = ["mpg123", "-"], stdin = subprocess.PIPE, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-        self.start_time = None
-        self.bytecount = 0
-        self.logger = logging.getLogger("MP3Player")
+        self._receivers = []
+    def connect(self, receiver):
+        self._receivers.append(receiver)
+    def emit(self, *args, **kwargs):
+        for receiver in self._receivers:
+            receiver(*args, **kwargs)
+
+class AudioStreamPlayer:
+    """ A basic audioplayer class that pushes data to a subprocess audio player.
+    """
+    def __init__(self, executable, executable_options):
+
+        self._logger = logging.getLogger(executable)
+
+        self._executable = executable
+
+        self._logger.debug("Starting {!r} sub-process ...".format(self._executable))
+
+        args = [executable] + [option for option in executable_options]
+
+        self._process = subprocess.Popen(args = args, stdin = subprocess.PIPE, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+
+        self._last_report_time = float("-inf")
+        self._bytecount = 0
+        self._start_time = time.time()
+
     def __del__(self):
-        self.process.stdin.close()
-        self.process.wait()
+
+        if self._process is not None:
+            self._logger.error("The__del__ method of class AudioStreamPlayer was called while the subprocess was still active. Please use explicit close() method.")
+            self.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self._process is not None:
+            self.close()
+
+    def close(self):
+
+        assert self._process is not None
+
+        self._logger.debug("Stopping {!r} sub-process.".format(self._executable))
+        self._process.wait()
+        self._process = None
+
     def play(self, data):
 
         current_time = time.time()
 
-        self.process.stdin.write(data)
+        self._process.stdin.write(data)
 
-        if self.start_time is None:
-            self.start_time = current_time
-    
-        old_bytecount = self.bytecount
-        self.bytecount += len(data)
+        self._bytecount += len(data)
 
-        REPORT_INTERVAL = 1048576
+        REPORT_INTERVAL_SECONDS = 30.0
 
-        if (self.bytecount // REPORT_INTERVAL) > (old_bytecount // REPORT_INTERVAL):
+        if current_time - self._last_report_time >= REPORT_INTERVAL_SECONDS:
 
-            megabytes = self.bytecount / 1048576.0
-            kilobits  = self.bytecount * 8.0 / 1000.0 # These 'kilobits' are 1000 bits.
+            megabytes = self._bytecount / 1048576.0
+            kilobits  = self._bytecount * 8.0 / 1000.0 # These 'kilobits' are 1000 bits.
 
-            seconds = current_time - self.start_time
+            seconds = current_time - self._start_time
             hours = seconds / 3600.0
 
             megabytes_per_hour  = megabytes / hours
             kilobits_per_second = kilobits / seconds
 
-            self.logger.info("Streamed {:.1f} MB in {:.3f} hours ({:.3f} MB/h, {:.3f} kbits/sec)".format(megabytes, hours, megabytes_per_hour, kilobits_per_second))
+            hms_s = seconds
+            hms_h = math.floor(seconds / 3600.0)
+            hms_s -= hms_h * 3600.0
+            hms_m = math.floor(seconds / 60.0)
+            hms_s -= hms_m * 60.0
+
+            self._logger.info("Streamed {:.3f} MB in {}h{:02}m{:06.3f}s ({:.3f} MB/h, {:.6f} kbits/sec)".format(megabytes, hms_h, hms_m, hms_s, megabytes_per_hour, kilobits_per_second))
+
+            self._last_report_time = current_time
 
 class MetadataProcessor:
 
     def __init__(self, led_device):
 
+        self.logger = logging.getLogger("metadata")
+
         self.led_device = led_device
 
         self.regexp = re.compile("StreamTitle='(.*)';StreamUrl='(.*)';")
 
-        ledz = LedDisplay(self.led_device)
-        ledz.setBrightnessLevel("A")
-        ledz.setSchedule("A", "A")
-        ledz.close()
+        if led_device is not None:
+            with LedDisplay(self.led_device) as led_display:
+                led_display.setBrightnessLevel("A")
+                led_display.setSchedule("A", "A")
 
-        self.logger = logging.getLogger("Metadata")
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
 
     def process(self, metadata):
 
@@ -70,22 +117,24 @@ class MetadataProcessor:
 
         self.logger.info("Stream title: {!r}".format(title))
 
-        message = "<L1><PA><FE><MA><WB><FE><AC><CD>{}          <CG><KD> <KT>".format(title)
+        if self.led_device is not None:
 
-        ledz = LedDisplay(self.led_device)
-        ledz.send(message)
-        ledz.close()
+            led_message = "<L1><PA><FE><MA><WB><FE><AC><CD>{}          <CG><KD> <KT>".format(title)
+
+            with LedDisplay(self.led_device) as led_display:
+                ledz.send(led_message)
 
 class InternetRadioPlayer:
 
-    def __init__(self, host, port, mp3player, metadata_processor):
+    def __init__(self, host, port):
+
+        self.logger = logging.getLogger("{}:{}".format(host, port))
 
         self.host = host
         self.port = port
-        self.mp3player = mp3player
-        self.metadata_processor = metadata_processor
 
-        self.logger = logging.getLogger("InternetRadioPlayer")
+        self.audiodata_signal = Signal()
+        self.metadata_signal = Signal()
 
     def play(self):
 
@@ -96,10 +145,10 @@ class InternetRadioPlayer:
         stream_socket = socket.create_connection(address)
 
         try:
-        
+
             request = "GET / HTTP/1.0\r\nHost:{}\r\nIcy-MetaData: 1\r\nAccept: */*\r\n\r\n".format(self.host).encode()
 
-            self.logger.info("Sending HTTP request: {!r} to {}:{}".format(request, self.host, self.port))
+            self.logger.info("Sending HTTP request: {!r} ...".format(request))
 
             stream_socket.send(request)
 
@@ -109,7 +158,7 @@ class InternetRadioPlayer:
 
             stream_buffer = bytearray()
             icy_metadata_interval = None
-
+            icy_content_type = None
 
             while True:
 
@@ -128,17 +177,21 @@ class InternetRadioPlayer:
                 # Discard header line and CR/LF from stream buffer.
                 del stream_buffer[:idx + 2]
 
-                self.logger.info("Header line: {!r}".format(headerline))
+                self.logger.info("Received response header: {!r}".format(headerline))
 
                 if headerline.startswith("icy-metaint:"):
+                    assert icy_metadata_interval is None
                     icy_metadata_interval = int(headerline[12:])
-
-                if len(headerline) == 0:
+                elif headerline.startswith("content-type:"):
+                    assert icy_content_type is None
+                    icy_content_type = headerline[13:]
+                elif len(headerline) == 0:
                     # Empty line terminates the header!
                     break
 
             assert icy_metadata_interval is not None
-            
+            assert icy_content_type      is not None
+
             # Stream MP3 data, interleaved with ICY metadata
 
             self.logger.debug("Interpret interleaved audio / ICY data.")
@@ -155,8 +208,7 @@ class InternetRadioPlayer:
 
                         stream_mp3_bytes = min(stream_mp3_bytes_until_metadata, len(stream_buffer))
 
-                        if self.mp3player is not None:
-                            self.mp3player.play(stream_buffer[:stream_mp3_bytes])
+                        self.audiodata_signal.emit(stream_buffer[:stream_mp3_bytes])
 
                         del stream_buffer[:stream_mp3_bytes]
                         stream_mp3_bytes_until_metadata -= stream_mp3_bytes
@@ -181,29 +233,27 @@ class InternetRadioPlayer:
                         if len(stream_buffer) >= 1 + metadata_size:
 
                             # We have a complete metadata chunk.
-                            
-                            # Process metadata, if a processor is defined
-                            if self.metadata_processor is not None:
-                                metadata = stream_buffer[1:1 + metadata_size]
 
-                                if len(metadata) > 0:
+                            metadata = stream_buffer[1:1 + metadata_size]
 
-                                    metadata = bytes(metadata.rstrip(b"\0"))
+                            if len(metadata) > 0:
 
-                                    with open("metadata.log", "a") as f:
-                                        print("{:20.9f} {!r}".format(current_time, metadata), file = f)
+                                metadata = bytes(metadata.rstrip(b"\0"))
 
-                                    try:
-                                        metadata = metadata.decode()
-                                    except:
-                                        self.logger.error("metadata decode error: {!r}".format(metadata))
-                                        metadata = None
+                                with open("metadata.log", "a") as f:
+                                    print("{:20.9f} {!r}".format(current_time, metadata), file = f)
 
-                                    if metadata is not None:
-                                        self.metadata_processor.process(metadata)
+                                try:
+                                    metadata = metadata.decode()
+                                except:
+                                    self.logger.error("metadata decode error: {!r}".format(metadata))
+                                    metadata = None
+
+                                if metadata is not None:
+                                    self.metadata_signal.emit(metadata)
 
                             # Discard the metadata chunk, and leave metadata parsing loop.
-                                    
+
                             del stream_buffer[:1 + metadata_size]
                             break
 
@@ -218,22 +268,33 @@ class InternetRadioPlayer:
 
 def main():
 
-    logging_format = "%(asctime)s | %(levelname)-10s | %(name)-20s | %(message)s"
+    logging_format = "%(asctime)s | %(levelname)-10s | %(name)-25s | %(message)s"
     logging.basicConfig(level = logging.INFO, format = logging_format)
 
     try:
 
-        mp3player = MP3Player()
-        metadata_processor = MetadataProcessor("/dev/ttyUSB0")
-
         #host = "as192.pinguinradio.com"
         #host = "pu192.pinguinradio.com"
         host = "pc192.pinguinradio.com"
+        #host = "pcaac.pinguinradio.com"
 
         port = 80
 
-        player = InternetRadioPlayer(host, port, mp3player, metadata_processor)
-        player.play()
+        radioPlayer = InternetRadioPlayer(host, port)
+
+        led_device = None
+
+        with AudioStreamPlayer("mpg123", ["-"]) as audiostream_player, \
+             MetadataProcessor(led_device) as metadata_processor:
+
+            radioPlayer.audiodata_signal.connect(audiostream_player.play)
+            radioPlayer.metadata_signal.connect(metadata_processor.process)
+
+            radioPlayer.play() # blocking call
+
+    except KeyboardInterrupt:
+
+        logging.info("Quitting due to user request.")
 
     finally:
 
@@ -241,4 +302,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
